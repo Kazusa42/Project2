6e1ccace9e5a8ffcbf66e458f87273695a5b2472
Kitahara Kazusa <94526092+Kazusa42@users.noreply.github.com>
2023-05-17 21:31:22 +0900

    Add files via upload

--- configure.py
+++ configure.py
@@ -3,9 +3,21 @@ Global variable configure
 Aiming to simplify the trainging, predicting and evaluating process between different dataset and weights
 """
 
+""" Training setting """
+
+INIT_EPOCH = 55
+FREEZE_EPOCH, UNFREZZEZ_EPOCH = 50, 300
+FREEZE_BATCH_SIZE, UNFREEZE_BATCH_SIZE = 32, 4
+
+
 """  Basic configure for model """
+
 CLASSES_PATH = r'model_data/DOTA_classes.txt'
-MODEL_PATH = r'logs/last_epoch_weights.pth'
+
+if INIT_EPOCH == 0:
+    MODEL_PATH = r'model_data/yolox_m.pth'
+else:
+    MODEL_PATH = r'logs/last_epoch_weights.pth'
 
 INPUT_SHAPE = [640, 640]
 
@@ -14,16 +26,13 @@ Proposed methods
 1. Attention Neck
 2. Multi-scale crop
 """
-DATASET_PATH = r'DOTA/Single'
 
+DATASET_PATH = r'DOTA/Single'
 
 # traditional, none or light
 ATTEN_LIST = ['none', 'traditional', 'light']
 ATTEN_TYPE = ATTEN_LIST[2]
 
-""" Training setting """
-INIT_EPOCH = 156
-FREEZE_EPOCH, UNFREZZEZ_EPOCH = 50, 300
-FREEZE_BATCH_SIZE, UNFREEZE_BATCH_SIZE = 64, 8
+
 
 
